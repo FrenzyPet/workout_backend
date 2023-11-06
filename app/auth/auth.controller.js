373314b61245bf08bc56d8b9f0cf9418e5d86b99
frenzy <manpatbro@gmail.com>
2023-11-06 21:48:53 +0300

    create register func

--- app/auth/auth.controller.js
+++ app/auth/auth.controller.js
@@ -1,6 +1,43 @@
+import asyncHandler from 'express-async-handler'
+import { hash } from 'argon2'
+import { faker, tr } from '@faker-js/faker'
 import { prisma } from '../prisma.js'
+import { generateToken } from './generate-token.js'
+import { UserFields } from '../utils/user.utils.js'
 
-export const authUser = async (request, response) => {
-	const user = await prisma.user.findMany()
+export const authUser = asyncHandler(async (request, response) => {
+	const user = await prisma.user.findMany({
+		where: {
+			password1: 'qweqwe'
+		}
+	})
 	response.json(user)
-}
+})
+
+export const registerUser = asyncHandler(async (req, res) => {
+	const { email, password } = req.body
+
+	const isHaveUser = await prisma.user.findUnique({
+		where: {
+			email
+		}
+	})
+
+	if (isHaveUser) {
+		res.status(400)
+		throw new Error('A user with the same email already exists')
+	}
+
+	const user = await prisma.user.create({
+		data: {
+			email,
+			password: await hash(password),
+			name: faker.person.fullName()
+		},
+		select: UserFields
+	})
+
+	const token = generateToken(user.id)
+
+	res.json({ user, token })
+})
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import morgan from 'morgan'
 import authRoutes from './app/auth/auth.routes.js'
 import { prisma } from './app/prisma.js'
+import { errorHandler, notFound } from './app/middleware/error.middleware.js'
 
 dotenv.config()
 
@@ -16,6 +17,8 @@ async function main() {
 
 	app.use(express.json())
 	app.use('/api/auth', authRoutes)
+	app.use(notFound)
+	app.use(errorHandler)
 
 	const PORT = process.env.PORT || 7777
 
