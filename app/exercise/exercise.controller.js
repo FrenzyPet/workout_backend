ed564ccfc95475dc9e17788a87f7a7fbe07c00c6
frenzy <manpatbro@gmail.com>
2023-11-19 00:05:52 +0300

    create workout controller

--- app/exercise/exercise.controller.js
+++ app/exercise/exercise.controller.js
@@ -16,6 +16,53 @@ export const createNewExercise = asyncHandler(async (req, res) => {
 })
 
 export const getExercises = asyncHandler(async (req, res) => {
-	const exercises = await prisma.exercise.findMany()
+	const exercises = await prisma.exercise.findMany({
+		orderBy: {
+			createdAt: 'desc'
+		}
+	})
 	res.json(exercises)
+})
+
+export const updateExercise = asyncHandler(async (req, res) => {
+	const { name, times, iconPath } = req.body
+	const { id } = req.params
+
+	try {
+		const exercise = await prisma.exercise.update({
+			where: {
+				id: Number(id)
+			},
+			data: {
+				name,
+				times,
+				iconPath
+			}
+		})
+
+		res.json(exercise)
+	} catch (error) {
+		res.status(404)
+		throw new Error('Exercise not found!')
+	}
+})
+
+export const deleteExercise = asyncHandler(async (req, res) => {
+	const { id } = req.params
+
+	try {
+		const exercise = await prisma.exercise.delete({
+			where: {
+				id: Number(id)
+			}
+		})
+
+		res.json({
+			message: 'Exercise has been deleted',
+			exercise
+		})
+	} catch (error) {
+		res.status(404)
+		throw new Error('Exercise not found!')
+	}
 })
