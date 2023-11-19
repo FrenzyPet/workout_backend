3e545519e57207216ae7b806093f4df384be7fc5
frenzy <manpatbro@gmail.com>
2023-11-19 15:33:22 +0300

    create exercise-log controller

--- app/exercise/exercise.routes.js
+++ app/exercise/exercise.routes.js
@@ -1,11 +1,12 @@
 import express from 'express'
+import { protect } from '../middleware/auth.middleware.js'
 import {
 	createNewExercise,
 	deleteExercise,
 	getExercises,
 	updateExercise
 } from './exercise.controller.js'
-import { protect } from '../middleware/auth.middleware.js'
+import { createNewExerciseLog } from './log/exercise-log.controller.js'
 
 const router = express.Router()
 
@@ -15,5 +16,7 @@ router
 	.route('/:id')
 	.put(protect, updateExercise)
 	.delete(protect, deleteExercise)
+
+router.route('/log/:exerciseId').post(protect, createNewExerciseLog)
 
 export default router
